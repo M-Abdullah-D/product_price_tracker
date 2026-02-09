e955b11ae0f12103580f89a577c87bda8999d048
M-Abdullah-D <m.abdullah.dukmak@gmail.com>
2026-02-09 17:54:35 +0300

    Adding the website details

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ from paths import webdriver_path, browser_path, profile_path
 webdriver_path=webdriver_path
 browser_path=browser_path
 profile_path=profile_path
+
+# Adding the domain and the category pages to be scraped
+domain="https://www.amazon.com"
+book_shelf="Best-Sellers-Books-Business-Development-Entrepreneurship/zgbs/books/2741"
+page_num=["1","2"]
+for page in page_num:
+    website=f"{domain}/{book_shelf}/?_encoding=UTF8&pg={page}"

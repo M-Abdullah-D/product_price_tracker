f1ddc54a0163a3d1eeb050f1583cb5d4a6ec61dc
M-Abdullah-D <m.abdullah.dukmak@gmail.com>
2026-02-09 17:53:24 +0300

    Adding driver and browser paths

---
+++ app.py
@@ -0,0 +1,7 @@
+from paths import webdriver_path, browser_path, profile_path
+
+
+# Adding the path to the webdriver and the browser
+webdriver_path=webdriver_path
+browser_path=browser_path
+profile_path=profile_path

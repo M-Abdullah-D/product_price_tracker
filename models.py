f69baf32e96f3860bd27c2aa06056da2c00e4950
M-Abdullah-D <m.abdullah.dukmak@gmail.com>
2026-02-14 04:10:40 +0300

    class instead of lists

--- models.py
+++ models.py
@@ -1,13 +1,19 @@
+from dataclasses import dataclass, asdict
+from typing import List
+
+@dataclass
+class Book:
+    title: str = ""
+    author: str = ""
+    reviews_rate: str = ""
+    reviews_number: str = ""
+    kindle_price: str = ""
+    audiobook_price: str = ""
+    hardcover_price: str = ""
+    paperback_price: str = ""
+    url: str = ""
 
 
-Title_list = []
-Author_list = []
-Reviews_Rate_list = []
-Reviews_Number_list = []
-Kindle_price_list = []
-Audiobook_price_list = []
-Hardcover_price_list = []
-Paperback_price_list = []
-books_URLs = []
+Books = List[Book]
 
     
